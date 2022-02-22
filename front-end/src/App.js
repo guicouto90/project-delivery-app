9786dc6fd2bfc8fcd5babe0a54933ea23e174771
Gustavo <gu.bressan@outlook.com>
2022-02-22 15:41:41 -0300

    login page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Login from './pages/Login/Login';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/login" />
+        </Route>
+        <Route path="/login" component={ Login } />
+      </Switch>
     </div>
   );
 }

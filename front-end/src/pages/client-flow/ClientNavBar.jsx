42aa6607f11b02039605c61e347811cf9d201256
AC-Oliveira <allancardoso21@gmail.com>
2022-02-23 01:02:45 -0300

    Fix: Lint

--- front-end/src/pages/client-flow/ClientNavBar.jsx
+++ front-end/src/pages/client-flow/ClientNavBar.jsx
@@ -1,20 +1,40 @@
-import React from "react";
-import { useHistory, useLocation } from "react-router-dom";
+import React from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 function ClientNavBar() {
-  const history = useHistory()
+  const history = useHistory();
   const location = useLocation();
-  return(
+  return (
     <nav>
-      <button data-testId="customer_products__element-navbar-link-products" type="button">Produtos</button>
-      <button data-testId="customer_products__element-navbar-link-orders" type="button">Meus Pedidos</button>
-      <p data-testId="customer_products__element-navbar-user-full-name">Nome de Usuário</p>
-      <button date-testId="customer_products__element-navbar-link-logout" type="button" onClick={() => {
-        console.log(location)
-        history.push(`/login`);
-      } }>Sair</button>
+      <button
+        data-testId="customer_products__element-navbar-link-products"
+        type="button"
+      >
+        Produtos
+      </button>
+      <button
+        data-testId="customer_products__element-navbar-link-orders"
+        type="button"
+      >
+        Meus Pedidos
+      </button>
+      <p
+        data-testId="customer_products__element-navbar-user-full-name"
+      >
+        Nome de Usuário
+      </p>
+      <button
+        date-testId="customer_products__element-navbar-link-logout"
+        type="button"
+        onClick={ () => {
+          console.log(location);
+          history.push('/login');
+        } }
+      >
+        Sair
+      </button>
     </nav>
   );
-};
+}
 
 export default ClientNavBar;

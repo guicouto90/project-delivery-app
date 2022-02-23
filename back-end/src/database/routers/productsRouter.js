2b9505ee23694df5dc491fa12e5a9c354dd26987
adrianomonteiroweb <adrianomonteiroweb@gmail.com>
2022-02-23 17:30:45 -0300

    fix: image route correction and status code.

--- back-end/src/database/routers/productsRouter.js
+++ back-end/src/database/routers/productsRouter.js
@@ -4,11 +4,9 @@ const productsRouter = express.Router();
 const {
   getAllProducts,
   getProductById,
-  getImageProductByName,
 } = require('../controllers/productsController');
 
 productsRouter.get('/', getAllProducts);
 productsRouter.get('/:id', getProductById);
-productsRouter.get('/images/:name', getImageProductByName);
 
 module.exports = productsRouter;
